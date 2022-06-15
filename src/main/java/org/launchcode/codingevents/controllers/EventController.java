970d5bb81e49d44f6e0b9f84936ddea169efc743
Andie Johnson <ajohnson2@naic.org>
2022-06-15 09:37:54 -0500

    replace eventData wiht eventRepository

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,8 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,50 +19,56 @@ import javax.validation.Valid;
 @RequestMapping("events")
 public class EventController {
 
-    @GetMapping
-    public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
-        model.addAttribute("events", EventData.getAll());
-        return "events/index";
-    }
+	// tells spring boot to find an EventRepository object to put inside the eventRepository field
+	// finds the interface that has the @Repository annotation
+	// injects those objects wherever the @autowired annotation indicates
+	@Autowired
+	private EventRepository eventRepository;
 
-    @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
-        model.addAttribute(new Event());
-        model.addAttribute("types", EventType.values());
-        return "events/create";
-    }
+	@GetMapping
+	public String displayAllEvents(Model model) {
+		model.addAttribute("title", "All Events");
+		model.addAttribute("events", eventRepository.findAll());
+		return "events/index";
+	}
 
-    @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
-                                         Errors errors, Model model) {
-        if(errors.hasErrors()) {
-            model.addAttribute("title", "Create Event");
-            return "events/create";
-        }
+	@GetMapping("create")
+	public String displayCreateEventForm(Model model) {
+		model.addAttribute("title", "Create Event");
+		model.addAttribute(new Event());
+		model.addAttribute("types", EventType.values());
+		return "events/create";
+	}
 
-        EventData.add(newEvent);
-        return "redirect:";
-    }
+	@PostMapping("create")
+	public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+	                                     Errors errors, Model model) {
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "Create Event");
+			return "events/create";
+		}
 
-    @GetMapping("delete")
-    public String displayDeleteEventForm(Model model) {
-        model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
-        return "events/delete";
-    }
+		eventRepository.save(newEvent);
+		return "redirect:";
+	}
 
-    @PostMapping("delete")
-    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+	@GetMapping("delete")
+	public String displayDeleteEventForm(Model model) {
+		model.addAttribute("title", "Delete Events");
+		model.addAttribute("events", eventRepository.findAll());
+		return "events/delete";
+	}
 
-        if (eventIds != null) {
-            for (int id : eventIds) {
-                EventData.remove(id);
-            }
-        }
+	@PostMapping("delete")
+	public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
 
-        return "redirect:";
-    }
+		if (eventIds != null) {
+			for (int id : eventIds) {
+				eventRepository.deleteById(id);
+			}
+		}
+
+		return "redirect:";
+	}
 
 }
--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -4,6 +4,9 @@ import org.launchcode.codingevents.models.Event;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// we don't have to have a class that implements this EventRepository although you cannot create an instance of this interface (or any interfaces)
+// because CrudRepository is special because it's so boilerplate, and Spring Boot in-memory on the fly will create a class that implements the EventRepository
+// then Spring Boot will make sure it can be autowired into the field holding it
 @Repository
 public interface EventRepository extends CrudRepository<Event, Integer> {
 }
