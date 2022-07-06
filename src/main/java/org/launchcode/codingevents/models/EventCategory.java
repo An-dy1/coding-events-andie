e1c2f7145e02bdc143aa68716f4212c85b2c5cb4
Andie Johnson <ajohnson2@naic.org>
2022-07-06 11:37:11 -0500

    completed orm relationships part one lecture

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,7 +3,10 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -15,7 +18,10 @@ public class EventCategory extends AbstractEntity {
 	@Size(min = 3, message = "Name must be at least 3 characters long")
 	private String name;
 
-	public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
+	@OneToMany(mappedBy = "eventCategory")
+	private final List<Event> events = new ArrayList<>();
+
+	public EventCategory(String name) {
 		this.name = name;
 	}
 
@@ -30,6 +36,9 @@ public class EventCategory extends AbstractEntity {
 		this.name = name;
 	}
 
+	public List<Event> getEvents() {
+		return events;
+	}
 
 	@Override
 	public String toString() {
