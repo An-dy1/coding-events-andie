4e7d375420fd3f74e72b173269c3a4697e9621c6
Andie Johnson <ajohnson2@naic.org>
2022-07-14 17:50:26 -0600

    done establshing many to many relationship

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -24,106 +24,130 @@ import java.util.Optional;
 @RequestMapping("events")
 public class EventController {
 
-    @Autowired
-    private EventRepository eventRepository;
+	@Autowired
+	private EventRepository eventRepository;
 
-    @Autowired
-    private EventCategoryRepository eventCategoryRepository;
+	@Autowired
+	private EventCategoryRepository eventCategoryRepository;
 
-    @Autowired
-    private TagRepository tagRepository;
+	@Autowired
+	private TagRepository tagRepository;
 
-    @GetMapping
-    public String displayEvents(@RequestParam(required = false) Integer categoryId, Model model) {
+	@GetMapping
+	public String displayEvents(@RequestParam(required = false) Integer categoryId, Model model) {
 
-        if (categoryId == null) {
-            model.addAttribute("title", "All Events");
-            model.addAttribute("events", eventRepository.findAll());
-        } else {
-            Optional<EventCategory> result = eventCategoryRepository.findById(categoryId);
-            if (result.isEmpty()) {
-                model.addAttribute("title", "Invalid Category ID: " + categoryId);
-            } else {
-                EventCategory category = result.get();
-                model.addAttribute("title", "Events in category: " + category.getName());
-                model.addAttribute("events", category.getEvents());
-            }
-        }
+		if (categoryId == null) {
+			model.addAttribute("title", "All Events");
+			model.addAttribute("events", eventRepository.findAll());
+		} else {
+			Optional<EventCategory> result = eventCategoryRepository.findById(categoryId);
+			if (result.isEmpty()) {
+				model.addAttribute("title", "Invalid Category ID: " + categoryId);
+			} else {
+				EventCategory category = result.get();
+				model.addAttribute("title", "Events in category: " + category.getName());
+				model.addAttribute("events", category.getEvents());
+			}
+		}
 
-        return "events/index";
-    }
+		return "events/index";
+	}
 
-    @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
-        model.addAttribute(new Event());
-        model.addAttribute("categories", eventCategoryRepository.findAll());
-        return "events/create";
-    }
+	@GetMapping("create")
+	public String displayCreateEventForm(Model model) {
+		model.addAttribute("title", "Create Event");
+		model.addAttribute(new Event());
+		model.addAttribute("categories", eventCategoryRepository.findAll());
+		return "events/create";
+	}
 
-    @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
-                                         Errors errors, Model model) {
-        if (errors.hasErrors()) {
-            model.addAttribute("title", "Create Event");
-            return "events/create";
-        }
+	@PostMapping("create")
+	public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+	                                     Errors errors, Model model) {
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "Create Event");
+			return "events/create";
+		}
 
-        eventRepository.save(newEvent);
-        return "redirect:";
-    }
+		eventRepository.save(newEvent);
+		return "redirect:";
+	}
 
-    @GetMapping("delete")
-    public String displayDeleteEventForm(Model model) {
-        model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", eventRepository.findAll());
-        return "events/delete";
-    }
+	@GetMapping("delete")
+	public String displayDeleteEventForm(Model model) {
+		model.addAttribute("title", "Delete Events");
+		model.addAttribute("events", eventRepository.findAll());
+		return "events/delete";
+	}
 
-    @PostMapping("delete")
-    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+	@PostMapping("delete")
+	public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
 
-        if (eventIds != null) {
-            for (int id : eventIds) {
-                eventRepository.deleteById(id);
-            }
-        }
+		if (eventIds != null) {
+			for (int id : eventIds) {
+				eventRepository.deleteById(id);
+			}
+		}
 
-        return "redirect:";
-    }
+		return "redirect:";
+	}
 
-    @GetMapping("detail")
-    public String displayEventDetails(@RequestParam Integer eventId, Model model) {
+	@GetMapping("detail")
+	public String displayEventDetails(@RequestParam Integer eventId, Model model) {
 
-        Optional<Event> result = eventRepository.findById(eventId);
+		Optional<Event> result = eventRepository.findById(eventId);
 
-        if (result.isEmpty()) {
-            model.addAttribute("title", "Invalid Event ID: " + eventId);
-        } else {
-            Event event = result.get();
-            model.addAttribute("title", event.getName() + " Details");
-            model.addAttribute("event", event);
-        }
 
-        return "events/detail";
-    }
+		if (result.isEmpty()) {
+			model.addAttribute("title", "Invalid Event ID: " + eventId);
+		} else {
+			Event event = result.get();
+			model.addAttribute("title", event.getName() + " Details");
+			model.addAttribute("event", event);
+			model.addAttribute("tags", event.getTags());
+		}
 
-    @GetMapping("addTag")
-    public String displayAddTagToEventForm(@RequestParam Integer eventId, Model model) {
+		return "events/detail";
+	}
 
-        Optional<Event> result = eventRepository.findById(eventId);
-        Event event = result.get();
+	@GetMapping("add-tag")
+	public String displayAddTagToEventForm(@RequestParam Integer eventId, Model model) {
 
-        model.addAttribute("title", "Add tags to event " + event.getName());
-        model.addAttribute("tags", tagRepository.findAll());
+		Optional<Event> result = eventRepository.findById(eventId);
+		Event event = result.get();
 
-        // form needs to know which event to add the tag to
-        model.addAttribute("event", event);
+		model.addAttribute("title", "Add tags to event " + event.getName());
+		model.addAttribute("tags", tagRepository.findAll());
 
-        // allow us to validate a single object, instance of EventTagDTO
-        model.addAttribute(new EventTagDTO());
+		// form needs to know which event to add the tag to
+		EventTagDTO eventTag = new EventTagDTO();
+		eventTag.setEvent(event);
 
-        return "events/add-tag";
-    }
+		// allow us to validate a single object, instance of EventTagDTO
+		model.addAttribute("eventTag", eventTag);
+
+
+		return "events/add-tag";
+	}
+
+	@PostMapping("add-tag")
+	public String handleAddTagForm(@ModelAttribute @Valid EventTagDTO eventTag, Model model, Errors errors) {
+
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "Sorry, could not add tag, please try again");
+			return "redirect:/add-tag";
+		}
+
+		Event event = eventTag.getEvent();
+		Tag tag = eventTag.getTag();
+
+		if (!event.getTags().contains(tag)) {
+			event.addTag(tag);
+			eventRepository.save(event);
+		}
+
+		return "redirect:/events/detail?eventId=" + event.getId();
+
+	}
 
 }
