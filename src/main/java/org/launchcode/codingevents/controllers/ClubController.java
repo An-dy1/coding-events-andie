7f3f45feb7518dd986828326dc3bb8c3eff801a9
Andie Johnson <ajohnson2@naic.org>
2022-07-18 16:23:17 -0500

    added notes

--- src/main/java/org/launchcode/codingevents/controllers/ClubController.java
+++ src/main/java/org/launchcode/codingevents/controllers/ClubController.java
@@ -66,8 +66,9 @@ public class ClubController {
 		if (errors.hasErrors()) {
 			model.addAttribute("title", "Create admin for club: " + clubId);
 
+			// the model *should* already have any models that were bound (our ClubAdminDTO) and Errors - but that isn't happening
+			// because of a redirect? Because the clubAdminDTO isn't an entity itself?
 			model.addAttribute("errors", errors);
-
 			model.addAttribute("clubAndAdmin", clubAndAdminDTO);
 
 			return "clubs/createAdmin";
