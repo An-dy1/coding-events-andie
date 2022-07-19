3f2ce9483ac2dc2d8330ea5533cd922238dbe1d9
Andie Johnson <ajohnson2@naic.org>
2022-07-19 14:59:50 -0500

    login form and controller

--- src/main/java/org/launchcode/codingevents/controllers/UserController.java
+++ src/main/java/org/launchcode/codingevents/controllers/UserController.java
@@ -2,10 +2,19 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.UserRepository;
 import org.launchcode.codingevents.models.User;
+import org.launchcode.codingevents.models.dto.LoginFormDTO;
+import org.launchcode.codingevents.models.dto.RegistrationFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -43,6 +52,92 @@ public class UserController {
 	// set the user's id on the session's "user" attribute
 	private static void setUserInSession(HttpSession session, User user) {
 		session.setAttribute(userSessionKey, user.getId());
+	}
+
+	@GetMapping("/register")
+	public String displayRegistrationForm(Model model) {
+		model.addAttribute("registrationFormDTO", new RegistrationFormDTO());
+		model.addAttribute("title", "Register");
+		return "users/register";
+	}
+
+	@PostMapping("/register")
+	public String processRegistrationForm(
+			@ModelAttribute @Valid RegistrationFormDTO registrationFormDTO,
+			Errors errors,
+			Model model,
+			HttpServletRequest request) {
+
+		// if form errors, return to the form with the errors
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "Register");
+			return "users/register";
+		}
+
+		// otherwise, find the user in the repository
+		User existingUser = userRepository.findByUsername(registrationFormDTO.getUsername());
+
+		// if there's already a user there, don't register them, return 'user already exists error' and the registration form
+		if (existingUser != null) {
+			errors.rejectValue("username", "username.alreadyexists", "a user with that username already exists");
+			model.addAttribute("title", "Register");
+			return "users/register";
+		}
+
+		// otherwise, get the password and verifyPassword from the DTO
+		String password = registrationFormDTO.getPassword();
+		String verifyPassword = registrationFormDTO.getVerifyPassword();
+
+		// if they aren't equal, don't register them, return 'passwords don't match' and the registration form
+		if (!password.equals(verifyPassword)) {
+			errors.rejectValue("password", "passwords.mismatch", "passwords do not match");
+			model.addAttribute("title", "Register");
+			return "users/register";
+		}
+
+		// if the passwords are equal, then create a new User object using the value of username and (hashed) password
+		User newUser = new User(registrationFormDTO.getUsername(), registrationFormDTO.getPassword());
+		// save user in the database
+		userRepository.save(newUser);
+		// and set the "user" attribute in the current HTTP session with the new user
+		setUserInSession(request.getSession(), newUser);
+
+		return "redirect:";
+	}
+
+	@GetMapping("/login")
+	public String displayLoginForm(Model model) {
+
+		model.addAttribute(new LoginFormDTO());
+		model.addAttribute("title", "Login");
+		return "users/login";
+	}
+
+	@PostMapping("/login")
+	public String processLoginRequest(@Valid @ModelAttribute LoginFormDTO loginFormDTO, Errors errors, Model model, HttpServletRequest request) {
+
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "Login");
+			return "users/login";
+		}
+
+		User user = userRepository.findByUsername(loginFormDTO.getUsername());
+		
+		if (user == null) {
+			errors.rejectValue("username", "username.doesnotexist", "there is no user with that information");
+			model.addAttribute("title", "Login");
+			return "users/login";
+		}
+
+		if (!user.isMatchingPassword(loginFormDTO.getPassword())) {
+			errors.rejectValue("password", "password.incorrectpassword", "incorrect password");
+			model.addAttribute("title", "Login");
+			return "users/login";
+		}
+
+		setUserInSession(request.getSession(), user);
+
+		return "redirect:";
 	}
 }
 
